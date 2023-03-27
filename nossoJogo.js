44ac811da87afd42f678c2fc8d851d0afb32d77d
anabfer <abf.ferreirac@gmail.com>
2023-03-27 18:45:45 -0300

    Nosso Jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,31 @@
  * 
  * 
  */
+console.log('Boas vindas ao jogo de BlackJack')
+
+if(confirm('Quer iniciar uma nova rodada?')){
+   let cartaJogador1 = comprarCarta()
+   let cartaJogador2 = comprarCarta()
+
+   let cartaPc1 = comprarCarta()
+   let cartaPc2 = comprarCarta()
+
+   let pontosJogador = cartaJogador1.valor + cartaJogador2.valor
+   let pontosPc = cartaPc1.valor + cartaPc2.valor
+
+console.log(`Usuário cartas: ${cartaJogador1.texto} ${cartaJogador2.valor} - pontuação ${pontosJogador}`)
+console.log(`Computador - cartas: ${cartaPc1.texto} ${cartaPc2.valor} - pontuação ${pontosPc}`)
+
+if (pontosJogador > pontosPc) {
+   console.log("O usuário ganhou!")
+} else if (pontosPc > pontosJogador) {
+   console.log("O computador ganhou!")
+} else if (pontosJogador === pontosPc) {
+   console.log("Empate!")
+}
+}else {
+   console.log('O jogo acabou')
+}
+
+
+
